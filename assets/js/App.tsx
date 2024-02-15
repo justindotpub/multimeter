6e9cec2d6dcf20792e5cfd5af2467321a70ce065
Justin Johnson <justin@justin.pub>
2024-02-15 11:27:54 -0600

    TypeScript, React and MUI

---
+++ assets/js/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ButtonUsage from './ButtonUsage';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <ButtonUsage />
+    </div>
+  );
+}
+
+export default App;
